866b2335040f7d5bd34e1ce8cdd87544f7978023
Victor Yap <victoryapcheeann@gmail.com>
2018-07-06 01:26:39 +0800

    made some hanges to json file

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, function(){
+  console.log('Express server is up on port' + PORT);
+})
